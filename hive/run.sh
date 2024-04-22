3bca1215f25cd39260383a1c0539c782d523650b
Anh-Kiet Ngo <anhkiet@AK-Valerian.local>
2024-04-22 16:18:37 -0700

    Fixed configs

--- hive/run.sh
+++ hive/run.sh
@@ -6,5 +6,5 @@ then
   schematool -dbType postgres  -initSchema
 fi;
 
-hiveserver2 --service metastore &
-hiveserver2
+hiveserver2 --service metastore --hiveconf hive.root.logger=INFO,console --hiveconf yarn.root.logger=DEBUG,console --hiveconf hadoop.root.logger=DEBUG,console &
+hiveserver2 --hiveconf hive.root.logger=INFO,console --hiveconf yarn.root.logger=DEBUG,console --hiveconf hadoop.root.logger=DEBUG,console 
